3f55d403f90ea3af1bfbc10e3f22042147174116
Joanna <joanna@kiteswarms.com>
2019-06-20 13:04:14 +0200

    ble class

--- BLE_wcs/BLE_wcs.cpp
+++ BLE_wcs/BLE_wcs.cpp
@@ -4,13 +4,159 @@
     updates by chegewara
 */
 
-#include "C:\Users\Joanna\Documents\Arduino\libraries\BLE_wcs\BLE_wcs.h"
+#include "C:\Users\Joanna\Documents\MasterESE\3-Semester\WCS\wcs\BLE_wcs\BLE_wcs.h"
 
 
 
 
-void BLE_wcs::init_server()
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second. 
+*/
+
+
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+
+std::string property = "default";
+std::string value = "default";
+bool messageReceived = false;
+
+	std::string rxValue; 									//  global variable with received data
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+		deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+		deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+
+    void onWrite(BLECharacteristic *pCharacteristic) {
+		
+		rxValue = pCharacteristic->getValue();				// write received message to global variable rxValue
+		messageReceived = true;								// set messageReceived flag
+
+    }
+};
+
+bool BLE_wcs::getMessageReceived() {
+	return messageReceived;
+}
+
+void BLE_wcs::setMessageReceived(bool received) {
+	messageReceived = received;
+}
+
+void BLE_wcs::init(std::string deviceName)
 {
+	// Create the BLE Device
+	BLEDevice::init(deviceName);
+
+	// Create the BLE Server
+	BLEServer *pServer = BLEDevice::createServer();
+	pServer->setCallbacks(new MyServerCallbacks());
+
+	// Create the BLE Service
+	BLEService *pService = pServer->createService(SERVICE_UUID_UART);
+
+	// Create a BLE Characteristic
+	pCharacteristic = pService->createCharacteristic(
+						CHARACTERISTIC_UUID_TX,
+						BLECharacteristic::PROPERTY_NOTIFY
+						);
+
+
+	// create Descriptor for notify/indicate enable
+	pCharacteristic->addDescriptor(new BLE2902());
+
+	BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+	pCharacteristic->setCallbacks(new MyCallbacks());
+
+	// Start the service
+	pService->start();
+
+	// Start advertising
+	pServer->getAdvertising()->start();
+}
+
+void BLE_wcs::processMessage() {
+	if (rxValue.find(":") != -1) {
+		size_t cut = rxValue.find(":");
+			  
+	    std::string parameter = rxValue.substr(0, cut);
+		std::string sValue = rxValue.substr(cut, -1);
+		int value = atoi(sValue);
+	
+		if (parameter.compare("Skintype")) {
+			m_Skintype = value;
+		} else if (parameter.compare("UVthreshold")) {
+			m_UVthreshold = value;
+		} else if (parameter.compare("AQthreshold")) {
+			m_AQthreshold = value;
+			
+	} else if (rxValue.find("DataRequest") != -1) {
+		// init serial bluetooth
+	} else if (rxValue.find("get") != -1) {
+		char txString[20] = "AQthreshold: " + std::to_string(AQthreshold);
+		pCharacteristic->setValue(txString);
+		pCharacteristic->notify(); 						// Send the value
+	}
+}
+	
+
+
+void loop() {
+  if (deviceConnected) {
+    char txString[16] = "UV1000AQ100";
+    pCharacteristic->setValue(txString);
+    
+    pCharacteristic->notify(); // Send the value to the app!
+    Serial.print("*** Sent Value: ");
+    Serial.print(txString);
+    Serial.println(" ***");
+
+  }
+  delay(1000);
+}
+
+
+
+
+
+
+
+void BLE_wcs::init_server(void)
+{
+	/* Create Service and a separate Characteristic for each Sensor */
 	BLEDevice::init("Vitameter");
 	BLEServer *pServer = BLEDevice::createServer();
 	BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -33,5 +179,7 @@ void BLE_wcs::init_server()
 
 void BLE_wcs::init_client()
 {
+
+
 }
 
--- BLE_wcs/BLE_wcs.h
+++ BLE_wcs/BLE_wcs.h
@@ -3,12 +3,20 @@
 
 
 
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+#include "C:\Users\Joanna\Documents\MasterESE\3-Semester\WCS\wcs\ESP32_BLE\src\BLEDevice.h"
+#include "C:\Users\Joanna\Documents\MasterESE\3-Semester\WCS\wcs\ESP32_BLE\src\BLEUtils.h"
+#include "C:\Users\Joanna\Documents\MasterESE\3-Semester\WCS\wcs\ESP32_BLE\src\BLEServer.h"
+#include <BLE2902.h>
+#include <stdio.h>
+
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+#define SERVICE_UUID_UART      "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"   // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"   // RX characteristic UUID
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"   // TX characteristic UUID
+
 
 class BLE_wcs {
 	public:
@@ -16,10 +24,20 @@ class BLE_wcs {
 		BLE_wcs(void) {};
 		~BLE_wcs(void) {};
 		
-		void init_server();
-		void init_client();
+		// member functions
+		void init(std::string deviceName);
+		void init_server(void);
+		void init_client(void);
+		void setParameter();
+		std::string getProperty();
+		std::string getValue();
 		
 	private:
+		// member variables
+		int m_Skintype;
+		int m_UVtreshold;
+		int m_AQthreshold;
+		bool dataRequest;
 
 };
 
